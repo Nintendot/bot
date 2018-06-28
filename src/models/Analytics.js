48394e99398777635badf6b146c93f06e88eb3f9
Ben <ben.chen@telus.com>
2018-06-28 19:03:07 -0400

    fix: try to catch more logging for case neither interactive msg nor hearing

--- src/models/Analytics.js
+++ src/models/Analytics.js
@@ -16,16 +16,34 @@ export default class Analytics {
   }
 
   async save({ action, found, value}) {
-    return this.adapter.save({
-      team: this.message.team_id,
-      channel: this.message.channel,
-      channel_type: this.message.channel_type,
-      user: this.message.user,
-      method: this.message.type,
-      acronym: value || Analytics.decode(this.message.match[1]),
-      action,
-      found,
-      timestamp: this.message.event_time? moment.unix(this.message.event_time).format("YYYY-MM-DD hh:mm:ss") : moment.unix(this.message.action_ts.split('.')[0]).format("YYYY-MM-DD hh:mm:ss")
-    });
+    
+    try {
+      var ts;
+      if (this.message.event_time) { // event hearing
+        ts = moment.unix(this.message.event_time).format("YYYY-MM-DD hh:mm:ss");
+      } else if (this.message.action_ts) { // interactive msg
+        ts = moment.unix(this.message.action_ts.split('.')[0]).format("YYYY-MM-DD hh:mm:ss");
+      } else {
+        ts = '1900-00-00 00:00:00'; // default to a valid ts for db schema
+        console.log("Cannot find timestamp in message object, which was: ", message);
+        console.log("The action was: ", action);
+        console.log("The acronym was: ", value);
+      }
+      
+      return this.adapter.save({
+        team: this.message.team_id,
+        channel: this.message.channel,
+        channel_type: this.message.channel_type,
+        user: this.message.user,
+        method: this.message.type,
+        acronym: value || Analytics.decode(this.message.match[1]),
+        action,
+        found,
+        timestamp: ts
+      });
+
+    } catch(err) {
+      console.error("Analytics error: ", err);
+    }
   }
 }
