8c38bea1c6043b76f8c42ea186955ab2c8ecdb69
Ben <ben.chen@telus.com>
2018-08-02 17:00:48 -0400

    fix: do not rely on sql result for update as it's not reliable

--- src/storageAdapters/bigQueryAdapter.js
+++ src/storageAdapters/bigQueryAdapter.js
@@ -34,7 +34,7 @@ export default class bigQueryAdapter {
   }
 
   async save({ teamId, title, value, user, overwrite}) {
-      if (!overwrite) {
+      
         const exists = `select creator, definition from \`${teamId}.acronyms\` where acronym = '${title}' and creator = '${user}'`;
         const existsOptions = {
             query: exists,
@@ -43,42 +43,43 @@ export default class bigQueryAdapter {
         return await this.bigquery
         .query(existsOptions)
         .then(results => {
-            if(results[0].length > 0) { // use update instead
-                throw new Error(
-                    `Hi <@${user}>, It seems like you have already defined \`${title}\`.` + '\n' + `Please use the update command to modify its meaning:` + '\n' + `\`\`\`@Botcronym update ${title} to (new definition)\`\`\`` + '\n' + `For detail syntax, check out \`\`\`/@Botcronym help\`\`\``
-                );
-            } else { // save
-                const save = `insert into \`${teamId}.acronyms\` (acronym, creator, definition) values ('${title}', '${user}', '${value}')`;
-                const saveOptions = {
-                    query: save,
-                    useLegacySql: false, // Use standard SQL syntax for queries.
-                };
-                this.bigquery
-                .query(saveOptions)
-                .then(results => {
-                    return results[0];
-                })// Error will be caught and handled in outer stack
+            if(results[0].length > 0) { // found the record in DB
+                if (overwrite) { // acronym is already defined and action is to update it
+                    const update = `update \`${teamId}.acronyms\` set definition = '${value}' where acronym = '${title}' and creator = '${user}'`;
+                    const updateOptions = {
+                        query: update,
+                        useLegacySql: false, // Use standard SQL syntax for queries.
+                    };
+                    return this.bigquery
+                    .query(updateOptions)
+                    .then(results => {
+                        return results[0];
+                    })// Error will be caught and handled in outer stack
+
+                } else { // acronym is already defined but action is to define it
+                    throw new Error(
+                        `Hi <@${user}>, It seems like you have already defined \`${title}\`.` + '\n' + `Please use the update command to modify its meaning:` + '\n' + `\`\`\`@Botcronym update ${title} to (new definition)\`\`\`` + '\n' + `For detail syntax, check out \`\`\`/@Botcronym help\`\`\``
+                    );
+                }
+            } else { // no record found in DB
+                if (overwrite) { // acronym is not already defined but action is to update it
+                    throw new Error(
+                        `Hi <@${user}>, Doesn't look like you have defined \`${title}\` previously.` + '\n' + `Please use the define command to explain this acronym:` + '\n' + `\`\`\`@Botcronym ${title} means (definition)\`\`\`` + '\n' + `For detail syntax, check out \`\`\`/@Botcronym help\`\`\``
+                    );
+                } else { // acronym is not already defined and action is to define it
+                    const save = `insert into \`${teamId}.acronyms\` (acronym, creator, definition) values ('${title}', '${user}', '${value}')`;
+                    const saveOptions = {
+                        query: save,
+                        useLegacySql: false, // Use standard SQL syntax for queries.
+                    };
+                    return this.bigquery
+                    .query(saveOptions)
+                    .then(results => {
+                        return results[0];
+                    })// Error will be caught and handled in outer stack
+                }
             }
         })// Error will be caught and handled in outer stack
-    } else { // Overwrite
-        const update = `update \`${teamId}.acronyms\` set definition = '${value}' where acronym = '${title}' and creator = '${user}'`;
-        const updateOptions = {
-            query: update,
-            useLegacySql: false, // Use standard SQL syntax for queries.
-        };
-        return await this.bigquery
-        .query(updateOptions)
-        .then(results => {
-            if (results[0].length == 0) {
-                throw new Error(
-                    `Hi <@${user}>, Doesn't look like you have defined \`${title}\` previously.` + '\n' + `Please use the define command to explain this acronym:` + '\n' + `\`\`\`@Botcronym ${title} means (definition)\`\`\`` + '\n' + `For detail syntax, check out \`\`\`/@Botcronym help\`\`\``
-                );
-            }
-            else {
-                return results[0];
-            }
-        })// Error will be caught and handled in outer stack
-    }
   }
 
   async delete({teamId, title, user }) {
