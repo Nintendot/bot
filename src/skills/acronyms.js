83ef953b04f7ffbe97a9496eb517b44b86f7be28
Ben <ben.chen@telus.com>
2018-08-16 09:16:15 -0400

    fix: handle case where user is no longer in Slack team

--- src/skills/acronyms.js
+++ src/skills/acronyms.js
@@ -1,31 +1,30 @@
-import { WebClient } from '@slack/client';
+import { WebClient } from "@slack/client";
 
-import interactiveMsgCallback from './interactiveMsgCallback';
-import listenForActions from './listenForActions';
-import listenForAmbient from './listenForAmbient';
-import listenForDefinition from './listenForDefinition';
-import listenForDeletion from './listenForDeletion';
-import listenForQuestion from './listenForQuestion';
-import listenForUpdate from './listenForUpdate';
-import listenForSlash from './listenForSlash';
+import interactiveMsgCallback from "./interactiveMsgCallback";
+import listenForActions from "./listenForActions";
+import listenForAmbient from "./listenForAmbient";
+import listenForDefinition from "./listenForDefinition";
+import listenForDeletion from "./listenForDeletion";
+import listenForQuestion from "./listenForQuestion";
+import listenForUpdate from "./listenForUpdate";
+import listenForSlash from "./listenForSlash";
 
 //import Bigquery from '../analyticsAdapters/BigQuery';
-import localAnalytics from '../analyticsAdapters/localAnalytics';
-// import localDBAdapter from '../storageAdapters/localDBAdapter';
-import bigQueryAdapter from '../storageAdapters/bigQueryAdapter';
+import localAnalytics from "../analyticsAdapters/localAnalytics";
+// import localDBAdapter from "../storageAdapters/localDBAdapter";
+import bigQueryAdapter from "../storageAdapters/bigQueryAdapter";
 
-export default (controller) => {
-  const adapter = new bigQueryAdapter(controller.storage.teams)
-  // const adapter = new localDBAdapter(controller.storage.teams)
+export default controller => {
+  const adapter = new bigQueryAdapter(controller.storage.teams);
+  // const adapter = new localDBAdapter(controller.storage.teams);
   const analytics_adapter = new localAnalytics();
   // const web = new WebClient(process.env.slackToken);
 
   var listening = new Set();
   controller.storage.channels.all((error, channels) => {
-    console.log('init listening');
+    console.log("init listening");
     for (var channel of channels) if (channel.listen) listening.add(channel.id);
   });
-
 
   // Listening
   listenForUpdate(analytics_adapter)(controller);
--- src/skills/listenForQuestion.js
+++ src/skills/listenForQuestion.js
@@ -1,6 +1,6 @@
-import util from 'util';
-import Acronym from '../models/Acronym';
-import Analytics from '../models/Analytics';
+import util from "util";
+import Acronym from "../models/Acronym";
+import Analytics from "../models/Analytics";
 
 export default analytics_adapter => adapter => controller => {
   controller.hears(
@@ -9,9 +9,14 @@ export default analytics_adapter => adapter => controller => {
       /what(?:[^\w]|\si)s ([\w\.]+)\b/i,
       /what does (.+) (?:mean|stand for)\b/i
     ],
-    ['direct_message', 'mention', 'direct_mention'],
+    ["direct_message", "mention", "direct_mention"],
     async (bot, message) => {
-      bot.reply(message, `:ok_hand:, give me a second while I look up ${message.match[1]} :hourglass:`);
+      bot.reply(
+        message,
+        `:ok_hand:, give me a second while I look up ${
+          message.match[1]
+        } :hourglass:`
+      );
       const getUser = util.promisify(bot.api.users.info);
       const acronym = new Acronym({
         title: message.match[1],
@@ -25,36 +30,49 @@ export default analytics_adapter => adapter => controller => {
       try {
         const acronymData = await acronym.read();
         const msgs = await Promise.all(
-            acronymData.map(async (item) => {
-            const { user } = await getUser({user: item.creator});
-            return Object.assign({}, item, {creator: user.name});
+          acronymData.map(async item => {
+            let output = {};
+            try {
+              output = await getUser({ user: item.creator });
+            } catch (e) {
+              return Object.assign({}, item, {
+                creator: "unknown"
+              });
+            }
+            return Object.assign({}, item, { creator: output.user.name });
           })
         );
-        const formattedMsg = `Here are the results I found on acronym \`${message.match[1].toUpperCase()}\`:\n` + msgs.map(m => `${m.definition} by <@${m.creator}>`).join('\n');
-        bot.reply(
-          message,
-          formattedMsg
-        );
+        const formattedMsg =
+          `Here are the results I found on acronym \`${message.match[1].toUpperCase()}\`:\n` +
+          msgs
+            .map(m => {
+              if (m.creator != "unknown") {
+                return `\`\`\`${m.definition} by <@${m.creator}>\`\`\``;
+              } else {
+                return `\`\`\`${m.definition} by a former member\`\`\``;
+              }
+            })
+            .join("\n");
+        bot.reply(message, formattedMsg);
 
         // analytics - found result
         try {
           await analytics.save({
-            action: 'question',
+            action: "question",
             found: true
-          })
+          });
         } catch (e) {
           console.error(`Analytics error:`, e);
         }
-        
       } catch (e) {
         bot.reply(message, e.message);
 
         // analytics - no result
         try {
           await analytics.save({
-            action: 'question',
+            action: "question",
             found: false
-          })
+          });
         } catch (e) {
           console.error(`Analytics error:`, e);
         }
